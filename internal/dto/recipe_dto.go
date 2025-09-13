2cddc19458b4138a115d02b531647148001b6d0e
“arkiang” <adi@getstok.com>
2025-09-13 23:18:47 +0700

    handler and dto

---
+++ internal/dto/recipe_dto.go
@@ -0,0 +1,68 @@
+package dto
+
+// Request DTOs
+type CreateRecipeRequest struct {
+	Title       string `json:"title"`
+	MakingTime  string `json:"making_time"`
+	Serves      string `json:"serves"`
+	Ingredients string `json:"ingredients"`
+	Cost        int    `json:"cost"`
+}
+
+type UpdateRecipeRequest struct {
+	Title       string `json:"title"`
+	MakingTime  string `json:"making_time"`
+	Serves      string `json:"serves"`
+	Ingredients string `json:"ingredients"`
+	Cost        int    `json:"cost"`
+}
+
+// Response DTOs
+type CreateRecipeResponse struct {
+	Message string                    `json:"message"`
+	Recipe  []RecipeInfoWithTimestamp `json:"recipe"`
+}
+
+type GetRecipeResponse struct {
+	Message string     `json:"message,omitempty"`
+	Recipe  RecipeInfo `json:"recipe"`
+}
+
+type GetAllRecipesResponse struct {
+	Recipes []RecipeInfo `json:"recipes"`
+}
+
+type UpdateRecipeResponse struct {
+	Message string     `json:"message"`
+	Recipe  RecipeInfo `json:"recipe"`
+}
+
+type DeleteRecipeResponse struct {
+	Message string `json:"message"`
+}
+
+// RecipeInfo used inside response
+type RecipeInfo struct {
+	ID          int64  `json:"id,omitempty"`
+	Title       string `json:"title"`
+	MakingTime  string `json:"making_time"`
+	Serves      string `json:"serves"`
+	Ingredients string `json:"ingredients"`
+	Cost        int    `json:"cost"`
+}
+
+type RecipeInfoWithTimestamp struct {
+	ID          int64  `json:"id,omitempty"`
+	Title       string `json:"title"`
+	MakingTime  string `json:"making_time"`
+	Serves      string `json:"serves"`
+	Ingredients string `json:"ingredients"`
+	Cost        int    `json:"cost"`
+	CreatedAt   string `json:"created_at,omitempty"`
+	UpdatedAt   string `json:"updated_at,omitempty"`
+}
+
+type ValidationErrorResponse struct {
+	Message  string  `json:"message"`
+	Required *string `json:"required"`
+}
---
+++ internal/handler/recipe_handler.go
@@ -0,0 +1,194 @@
+package handler
+
+import (
+	"database/sql"
+	"errors"
+	"github.com/gofiber/fiber/v2"
+	"givery-recip/internal/common"
+	"givery-recip/internal/dto"
+	"givery-recip/internal/usecase"
+	"strconv"
+)
+
+type RecipeHandler struct {
+	uc usecase.RecipeUsecase
+}
+
+func NewRecipeHandler(app *fiber.App, uc usecase.RecipeUsecase) {
+	h := &RecipeHandler{uc}
+
+	app.Post("/recipes", h.Create)
+	app.Get("/recipes/:id", h.GetByID)
+	app.Get("/recipes", h.GetList)
+	app.Patch("/recipes/:id", h.Update)
+	app.Delete("/recipes/:id", h.Delete)
+}
+
+func (h *RecipeHandler) Create(c *fiber.Ctx) error {
+	var recipe dto.CreateRecipeRequest
+	if err := c.BodyParser(&recipe); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(dto.ValidationErrorResponse{
+			Message: err.Error(),
+		})
+	}
+
+	id, err := h.uc.Create(&recipe)
+	if err != nil {
+		var e *common.ValidationError
+		switch {
+		case errors.As(err, &e):
+			return c.Status(fiber.StatusBadRequest).JSON(dto.ValidationErrorResponse{
+				Message:  "Recipe creation failed!",
+				Required: &e.Missing,
+			})
+		default:
+			return c.Status(fiber.StatusInternalServerError).JSON(dto.ValidationErrorResponse{
+				Message: err.Error(),
+			})
+		}
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"id": id})
+}
+
+func (h *RecipeHandler) GetByID(c *fiber.Ctx) error {
+	idStr := c.Params("id")
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(dto.ValidationErrorResponse{
+			Message: "invalid recipe ID",
+		})
+	}
+
+	recipe, err := h.uc.GetByID(id)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(dto.ValidationErrorResponse{
+			Message: err.Error(),
+		})
+	}
+
+	if recipe == nil {
+		return c.Status(fiber.StatusNotFound).JSON(dto.ValidationErrorResponse{
+			Message: "recipe not found",
+		})
+	}
+
+	resp := dto.GetRecipeResponse{
+		Message: "Recipe details by id",
+		Recipe: dto.RecipeInfo{
+			ID:          recipe.ID,
+			Title:       recipe.Title,
+			MakingTime:  recipe.MakingTime,
+			Serves:      recipe.Serves,
+			Ingredients: recipe.Ingredients,
+			Cost:        recipe.Cost,
+		},
+	}
+
+	return c.JSON(resp)
+}
+
+func (h *RecipeHandler) GetList(c *fiber.Ctx) error {
+	recipes, err := h.uc.GetList()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(dto.ValidationErrorResponse{
+			Message: err.Error(),
+		})
+	}
+
+	resp := dto.GetAllRecipesResponse{
+		Recipes: make([]dto.RecipeInfo, len(recipes)),
+	}
+
+	for i, r := range recipes {
+		resp.Recipes[i] = dto.RecipeInfo{
+			ID:          r.ID,
+			Title:       r.Title,
+			MakingTime:  r.MakingTime,
+			Serves:      r.Serves,
+			Ingredients: r.Ingredients,
+			Cost:        r.Cost,
+		}
+	}
+
+	return c.JSON(resp)
+}
+
+func (h *RecipeHandler) Update(c *fiber.Ctx) error {
+	idStr := c.Params("id")
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(dto.ValidationErrorResponse{
+			Message: "invalid recipe ID",
+		})
+	}
+
+	var recipe dto.UpdateRecipeRequest
+	if err := c.BodyParser(&recipe); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(dto.ValidationErrorResponse{
+			Message: err.Error(),
+		})
+	}
+
+	update, err := h.uc.Update(id, &recipe)
+	if err != nil {
+		var e *common.ValidationError
+		switch {
+		case errors.As(err, &e):
+			return c.Status(fiber.StatusBadRequest).JSON(dto.ValidationErrorResponse{
+				Message:  "Recipe update failed!",
+				Required: &e.Missing,
+			})
+		default:
+			return c.Status(fiber.StatusInternalServerError).JSON(dto.ValidationErrorResponse{
+				Message: err.Error(),
+			})
+		}
+	}
+
+	if update == nil {
+		return c.Status(fiber.StatusNotFound).JSON(dto.ValidationErrorResponse{
+			Message: "recipe not found",
+		})
+	}
+
+	resp := dto.UpdateRecipeResponse{
+		Message: "Recipe successfully updated!",
+		Recipe: dto.RecipeInfo{
+			ID:          update.ID,
+			Title:       update.Title,
+			MakingTime:  update.MakingTime,
+			Serves:      update.Serves,
+			Ingredients: update.Ingredients,
+			Cost:        update.Cost,
+		},
+	}
+
+	return c.JSON(resp)
+}
+
+func (h *RecipeHandler) Delete(c *fiber.Ctx) error {
+	idStr := c.Params("id")
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(dto.ValidationErrorResponse{
+			Message: "invalid recipe ID",
+		})
+	}
+
+	err = h.uc.Delete(id)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return c.Status(fiber.StatusNotFound).JSON(dto.ValidationErrorResponse{
+			Message: "No recipe found",
+		})
+	}
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(dto.ValidationErrorResponse{
+			Message: err.Error(),
+		})
+	}
+
+	return c.JSON(fiber.Map{"message": "Recipe successfully removed!"})
+}
